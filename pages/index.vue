55493aad0e0f35c3dd608c26b74dd4eadb10e03b
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-12-31 15:13:46 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="layer">
+                <img src="~/assets/img/pembuka.jpg" alt="" width="100%">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row pt-5">
+            <div class="jumbotron text-center">
+                <img src="/assets/img/kepsek.png" alt="dimas" width="200px" class="rounded-circle img-thumbnail" />
+                <h1 class="name">Kurniawan S.Pd, M.Pd</h1>
+
+                <h4 class="kepsek pt-4">Kepala SMK Negeri 4 Tasikmalaya</h4>
+                <p class="lead">terwujudnya lulusan yang cerdas, kompetitif, adaptif, produktif, dan ber imtak</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container color">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@500&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+.container {
+    max-width: 100%;
+    margin: 0;
+}
+
+h5,
+p {
+    font-family: "Poppins", serif;
+}
+
+.img-fluid {
+    margin-top: 57%;
+
+}
+
+/* .kepsek {
+    background-color: #0365AE;
+} */
+
+.bg {
+    background-color: #DFE5E5;
+    background-position: center top;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 800px;
+    width: 100%;
+}
+
+.card {
+    margin: auto;
+    margin-top: 7%;
+
+
+}
+
+.layer {
+    /* z-index: -3; */
+    padding: 0;
+}
+
+.layer img{
+    z-index: -4;
+}
+
+
+.bg{
+    padding: 0;
+}
+
+
+.jumbotron img{
+    background: #0365AE;
+    border: 3px solid black;
+}
+
+
+</style>
